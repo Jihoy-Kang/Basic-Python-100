85861dc8419fe9c1c38f9d9f9e9889324e191fe8
Jihyo Kang <luxuryjihyo@naver.com>
2022-01-12 13:49:02 +0900

    first commit

---
+++ Python basic1-8.py
@@ -0,0 +1,26 @@
+#Python basic algorithm
+
+#1 : Print Hello
+print("Hello")
+
+#2 : Print Hello World with space
+print("Hello World")
+
+#3 : Print Hello and World at different lines
+print("Hello")
+print("World")
+
+#4 : Print 'Hello' with single quotation mark
+print("'Hello'")
+
+#5 : Print "Hello World" with double quotation mark
+print('"Hello World"')
+
+#6 : Print "!@#$%^&*()'
+print("\"!@#$%^&*()'") 
+
+#7 : Print "C:\Download\'hello'.py"
+print('"C:\\Download\\\'hello\'.py"')
+
+#8 : Print print("Hello\nWorld")
+print("print(\"Hello\\nWorld\")")
