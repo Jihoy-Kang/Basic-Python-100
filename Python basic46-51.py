fcce39f7f60a341fe52159780cf10d77ec0d955f
Jihyo Kang <luxuryjihyo@naver.com>
2022-01-16 10:10:42 +0900

    fifth

---
+++ Python basic46-51.py
@@ -0,0 +1,36 @@
+#Python basic algorithm 46 - 51
+
+#46
+n = input()
+n = int(n)
+print(n<<1)
+
+#47
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a<<b)
+
+#48
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a<b)
+
+#49
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a==b)
+
+#50
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a<=b)
+
+#51
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a!=b)
