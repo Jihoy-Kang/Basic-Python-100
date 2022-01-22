edc11159a9a559a8e50528485c9502099f2ef7e2
Jihyo Kang <luxuryjihyo@naver.com>
2022-01-22 14:04:23 +0900

    22 Jan 2022

--- Python basic77-91.py
+++ Python basic77-91.py
@@ -1,4 +1,4 @@
-#Python basic algorithm 77 - 
+#Python basic algorithm 77 - 91
 
 #77
 n = int(input())
@@ -73,3 +73,73 @@ b = int(b)
 c = int(c)
 s = int(s)
 print(format(h*b*c*s/8/1024/1024, '.1f'),'MB')
+
+#85
+w, h, b = input().split()
+w = int(w)
+b = int(b)
+h = int(h)
+print(format(h*b*w/8/1024/1024, '.2f'),'MB')
+
+#86
+n = int(input())
+s = int(0)
+c = int(0)
+
+while True :
+    s = s + c
+    c = c + 1
+    if n <= s :
+        break
+print(s)
+
+#87
+n = int(input())
+i = int()
+
+for i in range(1, n+1) :
+    if i%3==0 :
+        continue
+    print(i, end=' ')
+
+#88
+a, d, n = input().split()
+a = int(a)
+d = int(d)
+n = int(n)
+
+for i in range(1, n) :
+    a = a + d
+print(a)
+
+#89
+a, d, n = input().split()
+a = int(a)
+d = int(d)
+n = int(n)
+
+for i in range(1, n) :
+    a = a * d
+    print(a)
+
+#90
+a, m, d, n = input().split()
+a = int(a)
+m = int(m)
+d = int(d)
+n = int(n)
+
+for i in range(1, n) :
+    a = a * m + d
+print(a)
+
+#91
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+d = 1
+
+while d%a != 0 or d%b != 0 or d%c != 0 :
+    d = d + 1
+print(d)
