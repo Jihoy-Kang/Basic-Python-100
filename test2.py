29a4c322dc3b411b2b7634d59d258b57c8f41f83
Jihyo Kang <luxuryjihyo@naver.com>
2022-01-26 19:42:10 +0900

    26 Jan 2022

--- test2.py
+++ test2.py
@@ -7,13 +7,20 @@ for i in range(10) :
 for i in range(10) :
     mz[i] = list(map(int, input().split()))
 
-d = 0
-for i in range(10) :
-    for j in range(10) :
-        if mz[i][j] == 0 :
-            mz[i][j] = 9
-       
-     
+r = int(1)
+d = int(1)
+if mz[r][d+1] == 0 :
+    mz[r][d] = 1
+    r = r + 1
+elif mz[r][d] == 1 :
+    d = d + 1
+
+
+
+
+
+
+        
 for i in range(10) :
     for j in range(10) :
         print(mz[i][j], end=' ')
